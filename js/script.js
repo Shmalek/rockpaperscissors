7b8357755868075a99b6a1cdce78972b00604a6e
Shahriz Malek <malekshahriz15@gmail.com>
2018-10-10 01:13:00 +0000

    starting of the project

--- js/script.js
+++ js/script.js
@@ -4,8 +4,41 @@
 
 //GLOBAL VARIABLES
 
-
+/*global $ */
 
 
 // DOCUMENT READY FUNCTION BELOW
 
+$(document).ready(function() {
+
+    $("#shoot").click(function(){
+
+        var userChoice = $("#userChoice").text($("#input").val());
+        
+        var random = Math.random();
+        var computerChoice;
+        
+        if(random > 0.3333 && random < .6666){
+            
+            computerChoice = "rock"
+            
+        } else if(random < .3333 && random > 0){
+            
+            computerChoice = "scissors"
+            
+        } else if(random > .6666 && random < 1){
+            
+            computerChoice = "paper"
+        }
+
+        $("#computerChoice").text(computerChoice);
+        
+        
+        
+        
+    });    
+    
+    
+    
+    
+});
